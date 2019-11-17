0796c94fd733abd19fc8f620829a99c459c0a53a
Tandi Samples <tandisamples@gmail.com>
2019-11-17 15:26:51 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,50 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+    doc = Nokogiri::HTML(html)
+    students = doc.css("div.roster-cards-container")
+    # student_loc = doc.css("p.student-location")
+    # student_profile = doc.attr("href")
+    student_hash = []
+
+    students.each do |student|
+      student.css("div.student-card a").each do |html|
+        student_name = html.css("h4.student-name").text
+        student_loc = html.css("p.student-location").text
+        
+        student_profile = html.attr("href")
+        student_hash << {
+          :name => student_name,
+          :location => student_loc,
+          :profile_url => student_profile
+        }
+      end
+    end
+    student_hash
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    social_url = doc.css("div.social-icon-container a")
+    student = {}
+    social_url.each do |a|
+      if a.attributes["href"].value.include?("twitter")
+        student[:twitter] = a.attributes["href"].value
+      elsif a.attributes["href"].value.include?("linkedin")
+        student[:linkedin] = a.attributes["href"].value
+      elsif a.attributes["href"].value.include?("github")
+        student[:github] = a.attributes["href"].value
+      else
+        student[:blog] = a.attributes["href"].value
+      end
+      student[:profile_quote] = doc.css("div.profile-quote").text.chomp
+      student[:bio] = doc.css("div.description-holder p").text
+      # binding.pry
+    end
+    student
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +6,30 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    # @name = name
+    # @location = location
+    # binding.pry
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @profile_url = student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    # binding.pry
+    students_array.each do |name|
+      self.new(name)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send("#{key}=", value)
+      # binding.pry
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
-
